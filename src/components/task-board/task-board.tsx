cbd81a1d3710ea28f216822991ec01128c0dbc96
Алексей Сергиенко <lehazyo@gmail.com>
2021-10-10 14:46:28 +0300

    Можно закрыть нижнюю панель, нажав Escape

--- src/components/task-board/task-board.tsx
+++ src/components/task-board/task-board.tsx
@@ -1,18 +1,28 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect } from 'react';
 import { Columns } from '../columns/columns';
 import { DetailsPanel } from '../details-panel/details-panel';
 import { TaskStore } from '../../mobx-store/task-store';
 import '../../common.scss';
 import './task-board.scss';
 
-const taskStoreTest = new TaskStore();
-export const TaskContext = createContext(taskStoreTest);
+const taskStore = new TaskStore();
+export const TaskContext = createContext(taskStore);
 
-export const TaskBoard = () => (
-  <TaskContext.Provider value={taskStoreTest}>
-    <section className="task-board-wrapper">
-      <Columns />
-      <DetailsPanel />
-    </section>
-  </TaskContext.Provider>
-);
+export const TaskBoard = () => {
+  useEffect(() => {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "Escape") {
+        taskStore.setSelectedId(null);
+      }
+    })
+  }, []);
+
+  return (
+    <TaskContext.Provider value={taskStore}>
+      <section className="task-board-wrapper">
+        <Columns />
+        <DetailsPanel />
+      </section>
+    </TaskContext.Provider>
+  );
+};
